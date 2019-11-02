8bc89ad6c5a04002124a155aa6183d7695b5ac13
linbanana <tommy12345689@yahoo.com.tw>
2019-11-02 10:54:59 +0800

    #1 update

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once("connMysql.php");
+  
+
+?>
+
+
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- 環境建置 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link href="font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/ican.css" rel="stylesheet" />
+    <!-- 環境建置 -->
+    <!-- w3 js -->
+    <script src="scripts/w3.js"></script>
+    <!-- w3 js -->
+
+    <title>ican~</title>
+</head>
+
+
+
+
+<!-- header -->
+<div w3-include-html="layouts/header.php"></div>
+<!-- header -->
+
+<!-- content -->
+<div w3-include-html="layouts/content.php"></div>
+<!-- content -->
+
+<!-- footer -->
+<div w3-include-html="layouts/footer.php"></div>
+<!-- footer -->
+
+
+ <!-- 環境建置 -->
+    <script src="script/jquery-3.4.1.slim.min.js"></script>
+    <script src="script/popper.min.js"></script>
+    <script src="script/bootstrap.min.js"></script>
+    <script src="script/ican.js"></script>
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <!--<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d49835d5bd6ff90"></script>-->    
+    <!-- 環境建置 -->
+    <!-- w3 js -->
+    <script>w3.includeHTML();</script>
+    <!-- w3 js -->
+</body>
+</html>
