504bc538087f82dbebaf3ff09223ec271f7c4953
h90011h <57278981+h90011h@users.noreply.github.com>
2019-11-08 13:55:12 +0800

    Create about.php

---
+++ about.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="zh-tw">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<!-- 環境建置 -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<link href="font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+	<link href="css/bootstrap.min.css" rel="stylesheet" />
+	<link href="css/ican.css" rel="stylesheet" />
+	<!-- 環境建置 -->
+	<!-- w3 js -->
+	<script src="scripts/w3.js"></script>
+	<!-- w3 js -->
+	<title>ican</title>
+</head>
+<body>
+  <style>
+/*about.php*/
+.about{
+    text-align: center;
+}
+
+.about .container{
+    padding: 40px;
+    border-bottom: 1px solid rgb(226, 182, 94);
+}
+
+.about .container title{
+    padding: 40px;
+    font-size: 27px;
+    text-align: center;
+}
+
+.about .container p{
+    padding: 10px;
+    font-size: 12px;
+    font-weight: 300;
+    color: #000;
+    display: inline-block;
+}
+/*about.php*/
+  </style>
+	<!-- header --> 
+  	<div w3-include-html="layouts/header.php"></div>
+    <!-- header -->
+    <div class="about" >
+	   <div class="container">
+         <h3 class="title"><strong>關於我們</strong></h3><br/>
+			
+			<p>蓮潭國際會館是地下2層，地上10層，並擁有211間時尚客房、17間專業的會議室、500人的國際會議廳、1000人的會展大廳、暨3家頂級美饌中西式餐廳的精緻飯店。會館位於高雄新市政中心，坐擁最美麗的蓮池潭湖景，鄰近1,000公尺內計有高鐵左營站、高雄捷運站、左營火車站、國道出口、巨蛋球場、漢神百貨、新光三越。<br/>
+            
+
+		    蓮潭國際會館為高雄最新穎時尚之會館，面對著數百公頃綠與水的景觀，建築上，將自然與綠的元素融進整個設計之中，讓您有身處於城市裡的綠洲氛圍，外觀上，柔和白色的色調元素讓旅客彷如置於城堡般風情，當您走進大廳更能感受到挑高格局的玻璃屋大廳，感受我們的細心服務。
+			211間頂級客房，包括行政樓層、商務樓層，及頂級套房，每間房間都有絕佳的視野，拉開窗簾即能飽覽窗外的湖景與市景；餐飲包括宴會廳、中餐廳、義法餐廳、酒吧、大廳吧、烘焙屋、花園餐廳，以及，擁有舒適的豪華水療SPA池與活力健身房。
+			蓮潭國際會館更是全國最頂級的會議會館，特別提供全台最完整之會議顧問，會議管家與秘書服務，獨棟的會議中心裡，除了擁有17間各式大中小型會議室、電腦學習中心、專業階梯教室、國際會議廳以及多功能視廳中心，同時還有一可容納500人之國際會議廳。滿足各式會議的需求。
+			蓮潭國際會館為台灣首府大學耗資五億元獨資興建，並由五星級飯店團隊籌備打造下，希望在強調簡樸、生態、自然、紓放、商務、慢活的氛圍下，提供最佳服務，讓每位旅客能有賓至如歸，並擁有難忘的絕佳體驗。</p><br />
+       </div>
+    </div>
+    <!-- Footer -->
+    <div w3-include-html="layouts/footer.php"></div>
+    <!-- Footer -->
+
+    <!-- 環境建置 -->
+    <script src="scripts/jquery-3.4.1.slim.min.js"></script>
+    <script src="scripts/popper.min.js"></script>
+    <script src="scripts/bootstrap.min.js"></script>
+    <script src="scripts/ican.js"></script>
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d49835d5bd6ff90"></script>
+    <!-- 環境建置 -->
+    <!-- w3 js -->
+    <script>w3.includeHTML();</script>
+    <!-- w3 js -->
+</body>
+</html>
