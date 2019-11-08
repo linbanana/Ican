5c34a0fb6c70bfda6e0ab844ff367d5094d65f9b
linbanana <tommy12345689@yahoo.com.tw>
2019-11-08 15:46:49 +0800

    Update about.php

--- about.php
+++ about.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html>
 <html lang="zh-tw">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<!-- 環境建置 -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-	<link href="font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
-	<link href="css/bootstrap.min.css" rel="stylesheet" />
-	<link href="css/ican.css" rel="stylesheet" />
-	<!-- 環境建置 -->
-	<!-- w3 js -->
-	<script src="scripts/w3.js"></script>
-	<!-- w3 js -->
-	<title>ican</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- 環境建置 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link href="font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/ican.css" rel="stylesheet" />
+    <!-- 環境建置 -->
+    <title>ican</title>
 </head>
 <body>
   <style>
@@ -41,9 +39,9 @@
 }
 /*about.php*/
   </style>
-	<!-- header --> 
-  	<div w3-include-html="layouts/header.php"></div>
-    <!-- header -->
+    <?php
+        include("layouts/header.php");
+    ?>
     <div class="aboutcontent" >
 	   <div class="container">
          <h3 class="title"><strong>關於我們</strong></h3><br/>
@@ -57,20 +55,18 @@
 			蓮潭國際會館為台灣首府大學耗資五億元獨資興建，並由五星級飯店團隊籌備打造下，希望在強調簡樸、生態、自然、紓放、商務、慢活的氛圍下，提供最佳服務，讓每位旅客能有賓至如歸，並擁有難忘的絕佳體驗。</p><br />
        </div>
     </div>
-    <!-- Footer -->
-    <div w3-include-html="layouts/footer.php"></div>
-    <!-- Footer -->
+    <?php
+        include("layouts/footer.php");
+    ?>
 
     <!-- 環境建置 -->
     <script src="scripts/jquery-3.4.1.slim.min.js"></script>
-    <script src="scripts/popper.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="scripts/umd/popper.min.js"></script>
     <script src="scripts/bootstrap.min.js"></script>
-    <script src="scripts/ican.js"></script>
+    <script type="text/javascript" src="scripts/ican.js"></script>
     <!-- Go to www.addthis.com/dashboard to customize your tools -->
     <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d49835d5bd6ff90"></script>
     <!-- 環境建置 -->
-    <!-- w3 js -->
-    <script>w3.includeHTML();</script>
-    <!-- w3 js -->
 </body>
 </html>
