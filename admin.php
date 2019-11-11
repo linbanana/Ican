d49c7082a8c6f996e6cbc131cec4265a10074c93
linbanana <tommy12345689@yahoo.com.tw>
2019-11-11 16:57:35 +0800

    update

--- admin.php
+++ admin.php
@@ -2,29 +2,29 @@
 require_once("connMysql.php");
 session_start();
 //檢查是否經過登入
-/*if(!isset($_SESSION["loginMember"]) || ($_SESSION["loginMember"]=="")){
-    header("Location: index.php");
+if(!isset($_SESSION["loginMember"]) || ($_SESSION["loginMember"]=="")){
+	header("Location: index.php");
 }
 //檢查權限是否足夠
 if($_SESSION["memberLevel"]=="member"){
-    header("Location: member_center.php");
+	header("Location: member_center.php");
 }
 //執行登出動作
 if(isset($_GET["logout"]) && ($_GET["logout"]=="true")){
-    unset($_SESSION["loginMember"]);
-    unset($_SESSION["memberLevel"]);
-    header("Location: index.php");
+	unset($_SESSION["loginMember"]);
+	unset($_SESSION["memberLevel"]);
+	header("Location: index.php");
 }
 //刪除會員
 if(isset($_GET["action"])&&($_GET["action"]=="delete")){
-    $query_delMember = "DELETE FROM memberdata WHERE m_id=?";
-    $stmt=$db_link->prepare($query_delMember);
-    $stmt->bind_param("i", $_GET["id"]);
-    $stmt->execute();
-    $stmt->close();
-    //重新導向回到主畫面
-    header("Location: member_admin.php");
-}*/
+	$query_delMember = "DELETE FROM memberdata WHERE m_id=?";
+	$stmt=$db_link->prepare($query_delMember);
+	$stmt->bind_param("i", $_GET["id"]);
+	$stmt->execute();
+	$stmt->close();
+	//重新導向回到主畫面
+	header("Location: member_admin.php");
+}
 //選取管理員資料
 $query_RecAdmin = "SELECT m_id, m_name, m_logintime FROM memberdata WHERE m_username=?";
 $stmt=$db_link->prepare($query_RecAdmin);
@@ -69,6 +69,12 @@ $total_pages = ceil($total_records/$pageRow_records);
     <link href="css/bootstrap.min.css" rel="stylesheet" />
     <link href="css/ican.css" rel="stylesheet" />
     <!-- 環境建置 -->
+    <script language="javascript">
+    function deletesure(){
+    if (confirm('\n您確定要刪除這個會員嗎?\n刪除後無法恢復!\n')) return true;
+    return false;
+}
+</script>
     <title>ican</title>
 </head>
 <body>
@@ -76,74 +82,16 @@ $total_pages = ceil($total_records/$pageRow_records);
     <?php
     include("layouts/header.php");
     ?>
-    <div class="adminwrapper" id="showsidebar">
-        <div class="row">
-        <button class="btn btn-primary" id="menu-toggle">
-            管<br>
-            理<br>
-            中<br>
-            心              
-        </button>
-        <div class="admincontent col">
-            <table width="780" border="0" align="center" cellpadding="4" cellspacing="0">
-  <tr>
-    <td class="tdbline"><img src="images/mlogo.png" alt="會員系統" width="164" height="67"></td>
-  </tr>
-  <tr>
-    <td class="tdbline"><table width="100%" border="0" cellspacing="0" cellpadding="10">
-      <tr valign="top">
-        <td class="tdrline"><p class="title">會員資料列表 </p>
-          <table width="100%" border="0" cellpadding="2" cellspacing="1" bgcolor="#F0F0F0">
-            <tr>
-              <th width="10%" bgcolor="#CCCCCC">&nbsp;</th>
-              <th width="20%" bgcolor="#CCCCCC"><p>姓名</p></th>
-              <th width="20%" bgcolor="#CCCCCC"><p>帳號</p></th>
-              <th width="20%" bgcolor="#CCCCCC"><p>加入時間</p></th>
-              <th width="20%" bgcolor="#CCCCCC"><p>上次登入</p></th>
-              <th width="10%" bgcolor="#CCCCCC"><p>登入</p></th>
-            </tr>
-            <?php while($row_RecMember=$RecMember->fetch_assoc()){ ?>
-            <tr>
-              <td width="10%" align="center" bgcolor="#FFFFFF"><p><a href="member_adminupdate.php?id=<?php echo $row_RecMember["m_id"];?>">修改</a><br>
-                <a href="?action=delete&id=<?php echo $row_RecMember["m_id"];?>" onClick="return deletesure();">刪除</a></p></td>
-              <td width="20%" align="center" bgcolor="#FFFFFF"><p><?php echo $row_RecMember["m_name"];?></p></td>
-              <td width="20%" align="center" bgcolor="#FFFFFF"><p><?php echo $row_RecMember["m_username"];?></p></td>
-              <td width="20%" align="center" bgcolor="#FFFFFF"><p><?php echo $row_RecMember["m_jointime"];?></p></td>
-              <td width="20%" align="center" bgcolor="#FFFFFF"><p><?php echo $row_RecMember["m_logintime"];?></p></td>
-              <td width="10%" align="center" bgcolor="#FFFFFF"><p><?php echo $row_RecMember["m_login"];?></p></td>
-            </tr>
-            <?php }?>
-          </table>          
-          <hr size="1" />
-          <table width="98%" border="0" align="center" cellpadding="4" cellspacing="0">
-            <tr>
-              <td valign="middle"><p>資料筆數：<?php echo $total_records;?></p></td>
-              <td align="right"><p>
-                  <?php if ($num_pages > 1) { // 若不是第一頁則顯示 ?>
-                  <a href="?page=1">第一頁</a> | <a href="?page=<?php echo $num_pages-1;?>">上一頁</a> |
-                <?php }?>
-                  <?php if ($num_pages < $total_pages) { // 若不是最後一頁則顯示 ?>
-                  <a href="?page=<?php echo $num_pages+1;?>">下一頁</a> | <a href="?page=<?php echo $total_pages;?>">最末頁</a>
-                  <?php }?>
-              </p></td>
-            </tr>
-          </table><p>&nbsp;</p>
-          </td>
-        <td width="200">
-        <div class="boxtl"></div><div class="boxtr"></div>
-        <div class="regbox">
-          <p class="heading"><strong>會員系統</strong></p>
-            <p><strong><?php echo $mname;?></strong>您好。<br>
-            本次登入的時間為：<br><?php echo $mlogintime;?></p>
-            <p align="center"><a href="member_adminupdate.php?id=<?php echo $mid;?>">修改資料</a> | <a href="?logout=true">登出系統</a></p>
-        </div>
-        <div class="boxbl"></div><div class="boxbr"></div></td>
-      </tr>
-    </table>
-
-        </div>
-
-        <div class="adminsidebar col">
+          <div>
+            <div class="adminsidebar" id="showsidebar">
+              <div>
+              <button class="btn btn-primary" id="menu-toggle">
+                  管<br>
+                  理<br>
+                  中<br>
+                  心              
+              </button>
+              </div>
             <div class="sidebar-heading">
                 <img src="images/logo.png">
             </div>
@@ -172,15 +120,72 @@ $total_pages = ceil($total_records/$pageRow_records);
                             </ul>
                         </li>
                     </ul>
-                </li>
-                
-                
+                </li>               
             </ul>
         </div>
+      </div>
 
+
+<table width="780" border="0" align="center" cellpadding="4" cellspacing="0">
+  <tr>
+    <td class="tdbline"><img src="images/mlogo.png" alt="會員系統" width="164" height="67"></td>
+  </tr>
+  <tr>
+    <td class="tdbline"><table width="100%" border="0" cellspacing="0" cellpadding="10">
+      <tr valign="top">
+        <td class="tdrline"><p class="title">會員資料列表 </p>
+          <table width="100%" border="0" cellpadding="2" cellspacing="1" bgcolor="#F0F0F0">
+            <tr>
+              <th width="10%" bgcolor="#CCCCCC">&nbsp;</th>
+              <th width="20%" bgcolor="#CCCCCC"><p>姓名</p></th>
+              <th width="20%" bgcolor="#CCCCCC"><p>帳號</p></th>
+              <th width="20%" bgcolor="#CCCCCC"><p>加入時間</p></th>
+              <th width="20%" bgcolor="#CCCCCC"><p>上次登入</p></th>
+              <th width="10%" bgcolor="#CCCCCC"><p>登入</p></th>
+            </tr>
+			<?php while($row_RecMember=$RecMember->fetch_assoc()){ ?>
+            <tr>
+              <td width="10%" align="center" bgcolor="#FFFFFF"><p><a href="member_adminupdate.php?id=<?php echo $row_RecMember["m_id"];?>">修改</a><br>
+                <a href="?action=delete&id=<?php echo $row_RecMember["m_id"];?>" onClick="return deletesure();">刪除</a></p></td>
+              <td width="20%" align="center" bgcolor="#FFFFFF"><p><?php echo $row_RecMember["m_name"];?></p></td>
+              <td width="20%" align="center" bgcolor="#FFFFFF"><p><?php echo $row_RecMember["m_username"];?></p></td>
+              <td width="20%" align="center" bgcolor="#FFFFFF"><p><?php echo $row_RecMember["m_jointime"];?></p></td>
+              <td width="20%" align="center" bgcolor="#FFFFFF"><p><?php echo $row_RecMember["m_logintime"];?></p></td>
+              <td width="10%" align="center" bgcolor="#FFFFFF"><p><?php echo $row_RecMember["m_login"];?></p></td>
+            </tr>
+			<?php }?>
+          </table>          
+          <hr size="1" />
+          <table width="98%" border="0" align="center" cellpadding="4" cellspacing="0">
+            <tr>
+              <td valign="middle"><p>資料筆數：<?php echo $total_records;?></p></td>
+              <td align="right"><p>
+                  <?php if ($num_pages > 1) { // 若不是第一頁則顯示 ?>
+                  <a href="?page=1">第一頁</a> | <a href="?page=<?php echo $num_pages-1;?>">上一頁</a> |
+                <?php }?>
+                  <?php if ($num_pages < $total_pages) { // 若不是最後一頁則顯示 ?>
+                  <a href="?page=<?php echo $num_pages+1;?>">下一頁</a> | <a href="?page=<?php echo $total_pages;?>">最末頁</a>
+                  <?php }?>
+              </p></td>
+            </tr>
+          </table><p>&nbsp;</p>
+          </td>
+        <td width="200">
+        <div class="boxtl"></div><div class="boxtr"></div>
+        <div class="regbox">
+          <p class="heading"><strong>會員系統</strong></p>
+            <p><strong><?php echo $mname;?></strong>您好。<br>
+            本次登入的時間為：<br><?php echo $mlogintime;?></p>
+            <p align="center"><a href="member_adminupdate.php?id=<?php echo $mid;?>">修改資料</a> | <a href="?logout=true">登出系統</a></p>
         </div>
-    </div>
-    
+        <div class="boxbl"></div><div class="boxbr"></div></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center" background="images/album_r2_c1.jpg" class="trademark">© 2016 eHappy Studio All Rights Reserved.</td>
+  </tr>
+</table>
     <?php
     include("layouts/footer.php");
     ?>
@@ -196,3 +201,6 @@ $total_pages = ceil($total_records/$pageRow_records);
     <!-- 環境建置 -->
 </body>
 </html>
+<?php
+	$db_link->close();
+?>
