3ba7f0e7fe2425ca855ed0a2f97355968712df22
linbanana <tommy12345689@yahoo.com.tw>
2019-11-26 18:30:47 +0800

    11/26

--- join.php
+++ join.php
@@ -56,10 +56,10 @@ if(isset($_POST["action"])&&($_POST["action"]=="join")){
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <!-- 環境建置 -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link href="\font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
-    <link href="\css/bootstrap.min.css" rel="stylesheet" />
-    <link href="\css/ican.css" rel="stylesheet" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/ican.css" rel="stylesheet" />
     <!-- 環境建置 -->
     <title>ican</title>
 </head>
@@ -200,11 +200,11 @@ if(isset($_POST["action"])&&($_POST["action"]=="join")){
     ?>
         
     <!-- 環境建置 -->
-    <script src="\scripts/jquery-3.4.1.slim.min.js"></script>
+    <script src="scripts/jquery-3.4.1.slim.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="\scripts/umd/popper.min.js"></script>
-    <script src="\scripts/bootstrap.min.js"></script>
-    <script type="text/javascript" src="\scripts/ican.js"></script>
+    <script src="scripts/umd/popper.min.js"></script>
+    <script src="scripts/bootstrap.min.js"></script>
+    <script type="text/javascript" src="scripts/ican.js"></script>
     <!-- Go to www.addthis.com/dashboard to customize your tools -->
     <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d49835d5bd6ff90"></script>
     <!-- 環境建置 -->
