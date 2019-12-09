fd7cde0fcefcf3285c958f6f405ffea3820a15fe
h90011h <57278981+h90011h@users.noreply.github.com>
2019-12-09 16:52:12 +0800

    Update travel.php

--- travel.php
+++ travel.php
@@ -16,7 +16,7 @@ $sql = "SELECT * FROM `traveldata`";
 $result = mysqli_query($db_link, $sql);
 $sql1 = "SELECT * FROM `traveldata` WHERE `t_class`=\"水上活動\"";
 $result1 = mysqli_query($db_link, $sql1);
-$sql2 = "SELECT * FROM `traveldata` WHERE `t_class`=\"冬季旅遊\"";
+$sql2 = "SELECT * FROM `traveldata` WHERE `t_class`=\"潮間帶探索\"";
 $result2 = mysqli_query($db_link, $sql2);
 if (isset($_POST["action"]) && ($_POST["action"] == "travel")) {
   $query_insert = "INSERT INTO traveldata (t_name) VALUES (?)";
@@ -43,56 +43,8 @@ if (isset($_POST["action"]) && ($_POST["action"] == "travel")) {
     <link href="css/bootstrap.min.css" rel="stylesheet" />
     <link href="css/ican.css" rel="stylesheet" />
     <!-- 環境建置 -->
-    <title>ican</title>
-  </head>
-
-  <body>
-    <form action="" method="POST" name="formJoin" id="formJoin" onSubmit="return checkForm();">
-
-      <p><strong>水上活動</strong>：
-        <!-- <input name="t_name" type="text" class="normalinput" id="t_name">-->
-
-        <select name="t_name">
-          <?php
-          for ($i = 1; $i <= mysqli_num_rows($result1); $i++) {
-            $row_result1 = mysqli_fetch_assoc($result1);
-
-            ?>
-            <option value="<?php echo $row_result1['t_name']; ?>"><?php echo $row_result1['t_name']; ?></option>
-          <?php } ?>
-
-        </select>
-
-        <font color="#FF0000">*</font><br>
-      </p>
-
-
-      <p><strong>冬季旅遊</strong>：
-        <!-- <input name="t_name" type="text" class="normalinput" id="t_name">-->
-
-        <select name="t_name">
-          <?php
-          for ($i = 1; $i <= mysqli_num_rows($result2); $i++) {
-            $row_result2 = mysqli_fetch_assoc($result2);
-
-            ?>
-            <option value="<?php echo $row_result2['t_name']; ?>"><?php echo $row_result2['t_name']; ?></option>
-          <?php } ?>
-
-        </select>
-
-        <font color="#FF0000">*</font><br>
-      </p>
-
-      <input name="action" type="hidden" id="action" value="travel">
-      <input class="btn btn-success btn-sm" type="submit" name="Submit2" value="前往結帳">
-      <input class="btn btn-info btn-sm" type="reset" name="Submit3" value="重設資料">
-      <input class="btn btn-primary btn-sm" type="button" name="Submit" value="回上一頁" onClick="window.history.back();">
-      </p>
-    </form>
-
-    <!-- 環境建置 -->
-    <script src="scripts/jquery-3.4.1.slim.min.js"></script>
+     <!-- 環境建置 -->
+     <script src="scripts/jquery-3.4.1.slim.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="scripts/umd/popper.min.js"></script>
     <script src="scripts/bootstrap.min.js"></script>
@@ -100,6 +52,181 @@ if (isset($_POST["action"]) && ($_POST["action"] == "travel")) {
     <!-- Go to www.addthis.com/dashboard to customize your tools -->
     <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d49835d5bd6ff90"></script>
     <!-- 環境建置 -->
+    <script type="text/javascript">
+      $(function() {
+
+        //$(".btn1").on("click", function() {
+          //$(".day2").appendTo(".day1");
+        //});
+        // $(".t_class").change(function(){
+        //  $(".travellist").show();
+        //  });
+        
+          $("#show").attr("src",$("#imgr img").attr("src"));
+          $(".t_name").val("").change(function(){
+            var val=$(this);
+            $("#show").hide(1000,function(){
+              $(this).attr("src",val.attr("src"));
+            }).show(1500);
+            //if($(this).val()=="小琉球海底觀光潛水船"){
+              //$("img").eq(0).show();
+           // }else if($(this).val()=="威尼斯海灘"){
+              //$("#imgr").children().eq(1).show();
+            }
+          );
+
+          $(function(){
+        $("#show").attr("src",$("#imgr img:first").attr("src"));
+      
+    $(".t_name").hover(function(){
+       
+        var img=$(this);
+        $("#big").hide(1500,function(){
+
+            $(this).attr("src",img.attr("src"));
+
+        }).show(2000);
+    });
+
+});
+          
+        
+      });
+    </script>
+    <title>ican</title>
+    <style>
+      #travel {
+
+        border: 1px solid rgb(226, 182, 94);
+      }
+
+      .day1 {}
+
+      .day2 {}
+
+      img {
+        width: 300px;
+        height: 200px;
+        display: none;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div id="travel">
+      <form id="home">
+        <input type="button" class="btn1" value="新增">
+      </form>
+      <form action="" method="POST" name="formJoin" id="formJoin" onSubmit="return checkForm();">
+        <div class="day1">
+          <select name="t_class" class="t_class">
+
+            <option value="水上活動">水上活動</option>
+            <option value="潮間帶探索">潮間帶探索</option>
+            <option value="特色景點">特色景點</option>
+            <option value="美食小吃">美食小吃</option>
+
+          </select>
+
+          <div class="travellist" style="">
+            <p><strong>水上活動</strong>：
+              <!-- <input name="t_name" type="text" class="normalinput" id="t_name">-->
+
+              <select name="t_name" class="t_name">
+                <?php
+                for ($i = 1; $i <= mysqli_num_rows($result1); $i++) {
+                  $row_result1 = mysqli_fetch_assoc($result1);
+
+                  ?>
+                  <option value="<?php echo $row_result1['t_name']; ?>"><?php echo $row_result1['t_name']; ?></option>
+                <?php } ?>
+
+              </select>
+
+              <font color="#FF0000">*</font><br>
+            </p>
+          </div>
+          <img id="show">
+          <ul id="imgr">
+            <li><img src="images/travel/water_1.jpg"></li>
+            <li><img src="images/travel/water_2.jpg"></li>
+          </ul>
+          <p><strong>潮間帶探索</strong>：
+            <!-- <input name="t_name" type="text" class="normalinput" id="t_name">-->
+
+            <select name="t_name">
+              <?php
+              for ($i = 1; $i <= mysqli_num_rows($result2); $i++) {
+                $row_result2 = mysqli_fetch_assoc($result2);
+
+                ?>
+                <option value="<?php echo $row_result2['t_name']; ?>"><?php echo $row_result2['t_name']; ?></option>
+              <?php } ?>
+
+            </select>
+
+            <font color="#FF0000">*</font><br>
+          </p>
+        </div>
+
+
+        <div class="day2">
+          <select name="t_class" class="">
+
+            <option value="水上活動">水上活動</option>
+            <option value="潮間帶探索">潮間帶探索</option>
+            <option value="特色景點">特色景點</option>
+            <option value="美食小吃">美食小吃</option>
+
+          </select>
+
+          <div class="travellist" style="">
+            <p><strong>水上活動</strong>：
+              <!-- <input name="t_name" type="text" class="normalinput" id="t_name">-->
+
+              <select name="t_name">
+                <?php
+                for ($i = 1; $i <= mysqli_num_rows($result1); $i++) {
+                  $row_result1 = mysqli_fetch_assoc($result1);
+
+                  ?>
+                  <option value="<?php echo $row_result1['t_name']; ?>"><?php echo $row_result1['t_name']; ?></option>
+                <?php } ?>
+
+              </select>
+
+              <font color="#FF0000">*</font><br>
+            </p>
+          </div>
+
+          <p><strong>冬季旅遊</strong>：
+            <!-- <input name="t_name" type="text" class="normalinput" id="t_name">-->
+
+            <select name="t_name">
+              <?php
+              for ($i = 1; $i <= mysqli_num_rows($result2); $i++) {
+                $row_result2 = mysqli_fetch_assoc($result2);
+
+                ?>
+                <option value="<?php echo $row_result2['t_name']; ?>"><?php echo $row_result2['t_name']; ?></option>
+              <?php } ?>
+
+            </select>
+
+            <font color="#FF0000">*</font><br>
+          </p>
+        </div>
+    </div>
+    <input name="action" type="hidden" id="action" value="travel">
+    <input class="btn btn-success btn-sm" type="submit" name="Submit2" value="前往結帳">
+    <input class="btn btn-info btn-sm" type="reset" name="Submit3" value="重設資料">
+    <input class="btn btn-primary btn-sm" type="button" name="Submit" value="回上一頁" onClick="window.history.back();">
+    </p>
+    </form>
+
+
+
+   
   </body>
 
   </html>
