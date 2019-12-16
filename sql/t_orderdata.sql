2c3f576bca97f5b31fba75318308204eb66e8dd4
linbanana <tommy12345689@yahoo.com.tw>
2019-12-16 09:25:54 +0800

    Update t_orderdata.sql

--- sql/t_orderdata.sql
+++ sql/t_orderdata.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2019-12-13 19:49:03
+-- 產生時間： 2019-12-16 02:25:42
 -- 伺服器版本： 10.4.8-MariaDB
--- PHP 版本： 7.3.10
+-- PHP 版本： 7.2.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,17 +31,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `t_orderdata` (
   `m_id` int(10) NOT NULL,
   `travel_1` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `travel_2` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+  `travel_2` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `travel_3` varchar(20) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- 傾印資料表的資料 `t_orderdata`
 --
 
-INSERT INTO `t_orderdata` (`m_id`, `travel_1`, `travel_2`) VALUES
-(0, '水上活動', '小琉球-小綠龜潛水'),
-(0, '烤肉', '品鮮火鍋烤肉吃到飽'),
-(0, '廟', '關廟幸山堂');
+INSERT INTO `t_orderdata` (`m_id`, `travel_1`, `travel_2`, `travel_3`) VALUES
+(0, '水上活動', '小琉球-小綠龜潛水', ''),
+(0, '烤肉', '品鮮火鍋烤肉吃到飽', ''),
+(0, '廟', '關廟幸山堂', ''),
+(1, '品鮮火鍋烤肉吃到飽', '蛤板灣', '蛤板灣'),
+(41, '品鮮火鍋烤肉吃到飽', '小琉球-小綠龜潛水', '蛤板灣');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
