0f15f51984fb27517e8193190e1badf2947d8990
linbanana <tommy12345689@yahoo.com.tw>
2019-12-17 16:19:14 +0800

    Update tesin.php

--- tesin.php
+++ tesin.php
@@ -115,62 +115,60 @@ margin-bottom:50px;
                  <?php 
 
                   if( isset($_GET["selecttype"]) ){  
-                    $s= $_GET['selecttype'];
-                    $in=$_GET['ind'];
-                    $out=$_GET['outda'];
-                    $searchroom="SELECT `r_model` FROM `roomdata` WHERE `r_id`
-                     NOT IN (SELECT `r_id` FROM `orderdata` 
-                     WHERE (`o_citime` >= '$in' AND `o_citime` <= '$out') or 
-                           (`o_cotime` >= '$in' AND `o_cotime` <= '$out')) AND
-                            `r_type`='$s'"; 
-                    $roommodellist = $db_link->query($searchroom);  //用db_link物件執行sql語法
-                  }  
-                  
-                  for($i=0;$i<$roommodellist->num_rows;$i++){
-                    $rsm=$roommodellist->fetch_assoc();
-                   // echo "<option value=".$rsm['r_model'].">".$rsm['r_model']."</option>";
-                   if ($rsm['r_model']=="商務套房"){
-                       showroom('images/room/business.jpg','商務套房','6000','4','平均16坪/ 53平方公尺/ 569平方英尺
-                       獨立臥房內附有1張210厘米 x 200厘米雙人床或2張105厘米 x 200厘米單人床
-                       獨立客廳內配有舒適沙發和坐墊
-                       寬敞的乾濕分離浴室及歐舒丹沐浴用品
-                       至多限加1床');
-                  }
-                  elseif ($rsm['r_model']=="總統套房") {
-                       showroom('images/room/president.jpg','總統套房','7000','4','最奢華的客房，非凡的空間設計搭配高級訂製傢俱，
-                       提供房客總統級的下榻禮遇以及頂級舒適的至高享受。 挑高五公尺的寬敞客廳與兩層樓高的全景落地窗，能將城市美景盡收眼底。');
-                   }
-                  elseif ($rsm['r_model']=="和式客房") {
-                       showroom('images/room/jp-style.jpg','和式客房','2400','4','脫掉鞋，走上和式地板，
-                       投入淨白床鋪的溫柔擁抱，讓靈魂沉潛，在純淨中甦醒過來。璞樹文旅中最簡約的房型，唯有一間。');
+                      $s= $_GET['selecttype'];
+                      $in=$_GET['ind'];
+                      $out=$_GET['outda'];
+                      $searchroom="SELECT `r_model` FROM `roomdata` WHERE `r_id`
+                       NOT IN (SELECT `r_id` FROM `orderdata` 
+                       WHERE (`o_citime` >= '$in' AND `o_citime` <= '$out') or 
+                             (`o_cotime` >= '$in' AND `o_cotime` <= '$out')) AND
+                              `r_type`='$s'"; 
+                      $roommodellist = $db_link->query($searchroom);  //用db_link物件執行sql語法
+                      
+                    
+                    for($i=0;$i<$roommodellist->num_rows;$i++){
+                      $rsm=$roommodellist->fetch_assoc();
+                     // echo "<option value=".$rsm['r_model'].">".$rsm['r_model']."</option>";
+                     if ($rsm['r_model']=="商務套房"){
+                         showroom('images/room/business.jpg','商務套房','6000','4','平均16坪/ 53平方公尺/ 569平方英尺
+                         獨立臥房內附有1張210厘米 x 200厘米雙人床或2張105厘米 x 200厘米單人床
+                         獨立客廳內配有舒適沙發和坐墊
+                         寬敞的乾濕分離浴室及歐舒丹沐浴用品
+                         至多限加1床');
                     }
-                  elseif ($rsm['r_model']=="豪華家庭房") {
-                       showroom('images/room/luxury-family.jpg','豪華家庭房','2400','4','明亮、柔美的空間配置，
-                       度假風情設計，盡享歡樂月眉假期，讓您闔家共用天倫之樂');
-                    }
-                  elseif ($rsm['r_model']=="經濟家庭房") {
-                    showroom('images/room/parity-family.jpg','經濟家庭房','2000','4','包含房內的獨立浴室，免治馬桶。');
-                    }
-                  elseif ($rsm['r_model']=="海洋世界主題套房") {
-                    showroom('images/room/Ocean.jpg','海洋世界主題套房','1500','1~2','房內風格採大海裡深層遨遊的感覺有在房間內彷彿置身海中的感覺');
-                    }
-                  elseif ($rsm['r_model']=="迪士尼主題套房") {
-                    showroom('images/room/disney.jpg','迪士尼主題套房','1500','1~2','迪士尼電影繽紛多彩的世界將躍出銀幕成為客房的設計主題，為遊客的度假時刻揮灑無盡驚喜。');
-                    }
-                  elseif ($rsm['r_model']=="漫威主題套房") {
-                    showroom('images/room/marvel.jpg','漫威主題套房','1500','1~2','以紐約風格打造，並利用多種藝術創作展現每一位漫威英雄的特色。');
-                    }
+                    elseif ($rsm['r_model']=="總統套房") {
+                         showroom('images/room/president.jpg','總統套房','7000','4','最奢華的客房，非凡的空間設計搭配高級訂製傢俱，
+                         提供房客總統級的下榻禮遇以及頂級舒適的至高享受。 挑高五公尺的寬敞客廳與兩層樓高的全景落地窗，能將城市美景盡收眼底。');
+                     }
+                    elseif ($rsm['r_model']=="和式客房") {
+                         showroom('images/room/jp-style.jpg','和式客房','2400','4','脫掉鞋，走上和式地板，
+                         投入淨白床鋪的溫柔擁抱，讓靈魂沉潛，在純淨中甦醒過來。璞樹文旅中最簡約的房型，唯有一間。');
+                      }
+                    elseif ($rsm['r_model']=="豪華家庭房") {
+                         showroom('images/room/luxury-family.jpg','豪華家庭房','2400','4','明亮、柔美的空間配置，
+                         度假風情設計，盡享歡樂月眉假期，讓您闔家共用天倫之樂');
+                      }
+                    elseif ($rsm['r_model']=="經濟家庭房") {
+                      showroom('images/room/parity-family.jpg','經濟家庭房','2000','4','包含房內的獨立浴室，免治馬桶。');
+                      }
+                    elseif ($rsm['r_model']=="海洋世界主題套房") {
+                      showroom('images/room/Ocean.jpg','海洋世界主題套房','1500','1~2','房內風格採大海裡深層遨遊的感覺有在房間內彷彿置身海中的感覺');
+                      }
+                    elseif ($rsm['r_model']=="迪士尼主題套房") {
+                      showroom('images/room/disney.jpg','迪士尼主題套房','1500','1~2','迪士尼電影繽紛多彩的世界將躍出銀幕成為客房的設計主題，為遊客的度假時刻揮灑無盡驚喜。');
+                      }
+                    elseif ($rsm['r_model']=="漫威主題套房") {
+                      showroom('images/room/marvel.jpg','漫威主題套房','1500','1~2','以紐約風格打造，並利用多種藝術創作展現每一位漫威英雄的特色。');
+                      }
 
-                  } 
+                    } 
 
-                  $time=(( strtotime($_GET['outda']) - strtotime($_GET['ind']) )/ (60*60*24)+1); //計算總天數  
-                  $_SESSION["r_type"]=$_GET['selecttype'];
-                  $_SESSION["ind"]=$_GET['ind'];
-                  $_SESSION["alda"]=$time; 
-                  $_SESSION["outda"]=$_GET['outda'];
-
-
-
+                    $time=(( strtotime($_GET['outda']) - strtotime($_GET['ind']) )/ (60*60*24)+1); //計算總天數  
+                    $_SESSION["r_type"]=$_GET['selecttype'];
+                    $_SESSION["ind"]=$_GET['ind'];
+                    $_SESSION["alda"]=$time; 
+                    $_SESSION["outda"]=$_GET['outda'];
+                  }                  
                  ?>
        
 </div>
