81e8e58c73e63dafb6ddec24a54e3f736e0af264
snowknife <snowknife1985@gmail.com>
2019-12-20 15:24:42 +0800

    Update active.php

--- active.php
+++ active.php
@@ -8,12 +8,11 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") { //如果是 POST 請求
         $roommodellist = $db_link->query($searchroom);  //用db_link物件執行sql語法
         for($i=0;$i<$roommodellist->num_rows;$i++){
             $rsm=$roommodellist->fetch_assoc();
-            $res .="<option value='".$rsm['r_model']."'>".$rsm['r_model']."</option>";
+            $res .="<option value=".$rsm['r_model'].">".$rsm['r_model']."</option>";
         }
 
-        echo json_encode(array(
-            'res' => $res     
-        ));
+        echo $res     
+        ;
 
     }
 
